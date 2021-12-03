657e5c9c20ba268f847a74f6da6be34fafeb61fd
Lucas <lucasperez702@gmail.com>
2021-12-03 16:26:09 +0100

    add BaseController

--- src/controller/BaseController.php
+++ src/controller/BaseController.php
@@ -1,8 +1,30 @@
 <?php
-namespace App\Controller;
 
-abstract class BaseController
+
+class BaseController
 {
+    protected $params;
+    protected $template = __DIR__ . './../views/template.php';
+    protected $viewsDir = __DIR__ . './../views/';
+
+    public function __construct(string $action, array $params = [])
+    {
+        $this->params = $params;
+
+        $method = 'execute' . ucfirst($action);
+        if(!is_callable([$this, $method])) {
+            throw new \RuntimeException('Action non definie sur ce module');
+        }
+        $this->$method();
+
+    }
+
+    /**
+     * @param string $title
+     * @param array $vars
+     * @param string $view
+     * @return mixed
+     */
     public function render(string $title, array $vars,string $view)
     {
         $view = $this->viewsDir . $view. '.php';
@@ -11,5 +33,4 @@ abstract class BaseController
         $content = ob_get_clean();
         return require $this->template;
     }
-
 }
